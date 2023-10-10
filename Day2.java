4a8501ddc7c65ee723179afbda9aaf0a488b83ee
NurulImaniar <146690389+NurulImaniar@users.noreply.github.com>
2023-10-10 17:52:21 +0800

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,20 @@
+
+package day2;
+public class Day2 {
+
+    public static void main(String[] args) {
+        // Kita akan membuat variabel
+        //Tipe data Integer
+        int umur = 18; //Assingment
+        System.out.println("nilai umur =" + umur );
+        
+        umur = 25;
+        System.out.println("nilai umur baru =" + umur);
+        
+        //Kita akan membuat Deklarasi
+        int angka; //Deklarasi
+        angka = 14;
+        System.out.println("Nilai angka =" + angka);
+    }
+    
+}
