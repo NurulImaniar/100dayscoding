3a157e01128d70d71dcbc5a57b1787052db0852b
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-10-11 15:15:10 +0800

    Add files via upload

---
+++ Day_3.java
@@ -0,0 +1,25 @@
+
+package day_3;
+
+public class Day_3 {
+
+    public static void main(String[] args) {
+       
+       String nama = "Nurul Imaniar";
+       String alamat = "Tapango";
+       byte umur = 18;
+       double tinggiBadan = 157.0;
+       boolean jomblo = true;
+       String angkatan = "2023"; 
+       String prodi = " informatika";
+      
+       System.out.println("Nama: " + nama);
+       System.out.println("Alamat: " + alamat);
+       System.out.println("Umur: " + umur);
+       System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+       System.out.println("jomblo: " + jomblo);
+       System.out.println("Angakatan  " + angkatan);
+        System.out.println("Program studi" + prodi);
+    }
+    
+}
