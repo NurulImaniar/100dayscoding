1869d5389220030b29cd0059085864b968dbaa70
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-10-15 17:06:28 +0800

    Day_7

---
+++ Day_7.java
@@ -0,0 +1,45 @@
+package day_7;
+public class Day_7 {
+    public static void main(String[] args) {
+        // Operator perbandingan
+        /*
+        == Sama dengan / senilai
+        != Tidak sama dengan
+        >  Lebih dari
+        >= Lebih dari atau sama dengan
+        <  Kurang dari
+        <= Kurang dari atau sama dengan
+        */
+        // true (benar)
+        // false (salah)
+        
+        long besar1,besar2;
+        besar1 = 7;
+        besar2 = 7;
+        
+        System.out.println(besar1 == besar2);
+         System.out.println(besar1 != besar2);
+          System.out.println(besar1 > besar2);
+           System.out.println(besar1 >= besar2);
+            System.out.println(besar1 < besar2);
+             System.out.println(besar1 <= besar2);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
