a25e8bce7c7591f8b80010d55c78d4f377c75979
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-10-20 11:40:47 +0800

    Day_12

---
+++ Day_12.java
@@ -0,0 +1,20 @@
+package day_11;
+import java.util.Scanner;
+public class Day_12 {
+public static void main(String[] args) {
+    
+     Scanner input = new Scanner(System.in);
+     System.out.print("Masukkan nilai : ");
+     int nilai = input.nextInt();
+     int kkm = 85;
+     String hasil = nilai >= kkm? "lulus":"tidak lulus";
+     System.out.println("Nilai Lulu :" + nilai);
+     System.out.println("Lulu dinyatakan :" + hasil);
+     
+     
+     
+     
+     
+     
+ }
+}
