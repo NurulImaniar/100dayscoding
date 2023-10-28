36bc93fbee3be078ef67cced2034d45bf06f1ca7
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-10-28 19:11:15 +0800

    Day_20

---
+++ Day_20.java
@@ -0,0 +1,27 @@
+package day_11;
+public class Day_20 {
+    public static void main(String[] args) {
+          //Increment
+          int angka = 12;
+          System.out.println("Post Increment");
+          System.out.println("================");
+          System.out.println("Isi variabel : "+ angka);
+          System.out.println("Isi variabel : "+ angka++);
+          System.out.println("Isi variabel : "+ angka);
+          
+          System.out.println(" ");
+          
+          int angka1 = 15;
+          System.out.println("Pre Increment");
+          System.out.println("================");
+          System.out.println("Isi variabel : "+ angka1);
+          System.out.println("Isi variabel : "+ ++angka1);
+          System.out.println("Isi variabel : "+ angka1);
+          
+          
+        /*Post Increment (nilai++), akan menampilkan hasilnya terlebih dahulu, baru nilai variabel angka naik 1 angka,
+          namun dengan Pre increment (angka++), variabel angka akan ditambahkan 1 angka, baru nilainya di tampilkan. */
+          
+    }
+    
+}
