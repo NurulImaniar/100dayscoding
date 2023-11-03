94bd0baf1971943904860db289e8bf01fa307d46
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-03 23:46:51 +0800

    Day_25

---
+++ Day_25.java
@@ -0,0 +1,15 @@
+package day_11;
+public class Day_25 {
+    public static void main(String[] args) {
+//Membuat pola persegi menggunakan operator perulangan (Lopping)
+        int jumlah = 4;
+        for (int nilai1 = 1; nilai1 <= jumlah; nilai1++) {
+            for (int nilai2 = 1; nilai2 < 4; nilai2++) {
+                System.out.print("o  ");
+            }
+            System.out.println("o ");
+        }
+        System.out.println(" ");
+    }
+    
+}
