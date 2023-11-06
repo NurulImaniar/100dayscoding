7c282aa0c816e2ecca95faf061e84ad44026b4c1
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-06 20:35:50 +0800

    Array 1 dimensi

---
+++ Day_29.java
@@ -0,0 +1,14 @@
+package day_11;
+
+public class Day_29 {
+    public static void main(String[] args) {
+  //Array / Larik ialah sebuah veriabel yang dapat di isi lebih dari 1 value.
+  
+        int[] nilai = {6,7,8,9}; 
+        String [] hewan = {"ikan", "kucing", "ayam", "kuda"};
+        System.out.println(nilai[0]);
+        System.out.println(hewan[2]);
+        
+    }
+    
+}
