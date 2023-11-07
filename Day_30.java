30577976d398333018c29c9a826cb529a3576d02
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-07 21:28:05 +0800

    Lopping pada Array

---
+++ Day_30.java
@@ -0,0 +1,15 @@
+package Day_30;
+
+public class Day_30 {
+    
+    public static void main(String[] args) {
+        int [] a = {6,7,8,9,3,4,5,0};
+        int Total = a.length;
+        for (int i = 0;i < Total; i++){
+            
+            System.out.print(a[i]+" ");
+            
+        }
+    }
+    
+}
