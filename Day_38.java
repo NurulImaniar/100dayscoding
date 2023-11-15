834ab4ed1a88d07362302563665551017cb1673f
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-15 20:06:02 +0800

    Array multidimensi pada java

---
+++ Day_38.java
@@ -0,0 +1,19 @@
+package Day_38;
+public class Day_38 {
+    public static void main(String[] args) {
+        
+        String [][] alamat = {
+            {"Hilman", "\t- Polmam"},
+            {"Ali", "\t- Makassar"},
+            {"Gian", "\t- Majene"}
+        };
+        System.out.print(alamat [0][0]);
+        System.out.println(alamat [0][1]);
+        System.out.print(alamat [1][0]);
+        System.out.println(alamat [1][1]);
+        System.out.print(alamat [2][0]);
+        System.out.println(alamat [2][1]);
+        
+    }
+    
+}
