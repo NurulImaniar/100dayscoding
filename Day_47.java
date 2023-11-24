62a2e1de77ccda6222548da76f01bd01733ec500
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-24 21:55:58 +0800

    Day_47

---
+++ Day_47.java
@@ -0,0 +1,25 @@
+package Day_46;
+import java.util.Scanner;
+public class Day_47 {
+    public static void main(String[] args) {
+        int angka;
+        Scanner input = new Scanner (System.in);
+        System.out.print("Masukkan angka : ");
+        angka = input.nextInt();
+        int hasil = angka;
+        System.out.println(angka+"! =");
+        for (int nilai = angka; nilai > 0; nilai--){
+            System.out.print(nilai);
+            if (nilai > 1){
+                System.out.print(" x ");
+            }
+            if (angka != nilai){
+                hasil *= nilai;
+            }
+        }
+        System.out.println("");
+        System.out.println("  = "+hasil);
+        
+    }
+    
+}
