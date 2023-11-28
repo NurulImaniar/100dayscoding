32effe9adff00d792bca7537c4e51b43e2483024
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-28 18:47:17 +0800

    Day_51

---
+++ Day_51.java
@@ -0,0 +1,19 @@
+package day_11;
+public class Day_12 {
+public static void main(String[] args) {
+    int [] nilai = {70,90,86,95,78};
+    
+    int jumlah = 0;
+    for (int i = 0; i < nilai.length; i++) {
+        jumlah += nilai[i];
+        System.out.println(jumlah);
+        
+    }
+    
+     
+     
+     
+     
+     
+ }
+}
