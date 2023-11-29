ddef012659f73b67ee5c5345c10cbccaac201476
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-11-29 22:37:06 +0800

    Day_52

---
+++ Day_52.java
@@ -0,0 +1,22 @@
+package Day_52;
+
+public class Day_52 {
+    
+    public static void main(String[] args) {
+        int[] nilai = {1, 2, 3};
+
+for (int i = 0; i < nilai.length; i++) {
+    if (nilai[i] == 1) {
+        System.out.println("Nilai adalah 1");
+    } else if (nilai[i] == 2) {
+        System.out.println("Nilai adalah 2");
+    } else if (nilai[i] == 3) {
+        System.out.println("Nilai adalah 3");
+    } else {
+        System.out.println("Nilai tidak dikenali");
+    }
+}
+
+    }
+    
+}
