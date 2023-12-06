4b38bf886e0eaf4757126f1fea87feca59352b25
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2023-12-06 23:01:36 +0800

    Latihan penerapan prosedur dan fungsi pada java

---
+++ day_59.java
@@ -0,0 +1,15 @@
+package day_59;
+public class day_59 {
+    public static void operasi (int a, int b){
+        int c ;
+        c = a + b;
+        System.out.print(a+" + "+b+" = "+c);
+    }
+    public static void main(String[] args) {
+        int a,b;
+        a = 18;
+        b = 37;
+        operasi(a,b);
+    }
+    
+}
